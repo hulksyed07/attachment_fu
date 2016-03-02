b730e39aae88506b2421639b7e4adc6f2dba340c
hulksyed07 <hulk.syed07@gmail.com>
2016-03-02 19:31:13 +0530

    required the file attachment_fu.rb
    Technoweenie::AttachmentFu::ActMethods was unable to load

--- lib/pothoven-attachment_fu.rb
+++ lib/pothoven-attachment_fu.rb
@@ -7,6 +7,7 @@ if defined?(Rails::Engine)
 
     initializer "attachment_fu" do
       require 'geometry'
+      require 'technoweenie/attachment_fu'
 
       ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
       Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
